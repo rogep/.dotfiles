34ab74bb6c43b0fad93f7687093de6dafd0cb843
Ephraim Rogers <Ephraim@Ephraims-MacBook-Pro.local>
2024-03-08 15:36:06 +1030

    feat: add new nvim config

---
+++ .config/nvim/lua/custom/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+  { 'ThePrimeagen/git-worktree.nvim' },
+  {
+    'telescope.nvim',
+    dependencies = {
+      'ThePrimeagen/git-worktree.nvim',
+      build = 'make',
+      config = function()
+        require('telescope').load_extension 'git_worktree'
+      end,
+    },
+  },
+}
